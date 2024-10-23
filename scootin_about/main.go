9b20a75fb8330207f43c7c28d77e8920b9b81bc9
nudabagana <matasramanuaskas@gmail.com>
2024-10-23 22:43:12 +0300

    gin added

--- scootin_about/main.go
+++ scootin_about/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, Dragon")
+	port, found := os.LookupEnv(`PORT`)
+	if !found {
+		port = "8090"
+	}
+
+	router := gin.New()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"*"},
+		AllowHeaders:  []string{"*"},
+		AllowWildcard: true,
+	}))
+
+	router.GET(`/health`, HealthGET)
+	router.Run(":" + port)
+}
+
+func HealthGET(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"version": "1.0.0",
+	})
 }
